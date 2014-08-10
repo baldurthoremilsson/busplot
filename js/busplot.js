75a6b1bc58eaf986595734543b7e03d6a29b0d12
Baldur Þór Emilsson <baldur@baldur.biz>
2014-08-10 21:52:21 +0000

    Adding "about" popup

--- js/busplot.js
+++ js/busplot.js
@@ -84,6 +84,110 @@ var BusControl = L.Control.extend({
 });
 
 
+var AboutPopup = L.Popup.extend({
+  content:
+    '<h1>BusPlot</h1>' +
+    '<p>Þessi síða birtir staðsetningu strætisvagna síðustu vikuna. ' +
+    'Hún velur 500 hnit af handahófi á hverri leið og teiknar inn á kortið. ' +
+    'Gögnin koma frá <a href="http://www.straeto.is">Strætó</a>, en síðan ' +
+    'tengist Strætó bs. ekki að öðru leiti.</p>' +
+    '<p>Höfundur er <a href="mailto:baldur@baldur.biz">Baldur Þór Emilsson</a> ' +
+    'og kóðann á bakvið síðuna má finna á ' +
+    '<a href="https://github.com/baldurthoremilsson/busplot">GitHub</a>.</p>',
+
+  loading: function() {
+    this.setContent(this.content +
+      '<p>Sæki gögn <img src="img/spinner.gif"></p>');
+  },
+
+  loaded: function() {
+    this.setContent(this.content);
+  },
+
+  loadingFailed: function(callback) {
+    var div = L.DomUtil.create('div');
+    div.innerHTML = this.content;
+
+    var span = L.DomUtil.create('span', '', div);
+    span.innerHTML = 'Gat ekki náð í gögnin. ';
+
+    var button = L.DomUtil.create('a', '', div);
+    button.innerHTML = 'Reyna aftur';
+    button.href = '#';
+    L.DomEvent
+      .on(button, 'mousedown dblclick', L.DomEvent.stopPropagation)
+      .on(button, 'click', L.DomEvent.stop)
+      .on(button, 'click', callback, this);
+
+    this.setContent(div);
+  },
+
+  onAdd: function(map) {
+    this.visible = true;
+    L.Popup.prototype.onAdd.call(this, map);
+  },
+
+  onRemove: function(map) {
+    this.visible = false;
+    L.Popup.prototype.onRemove.call(this, map);
+  },
+
+  _createButton: L.Control.Zoom.prototype._createButton,
+});
+
+
+var AboutControl = L.Control.extend({
+  options: {
+    position: 'topleft',
+  },
+
+  initialize: function() {
+    this.popup = new AboutPopup();
+    L.Control.prototype.initialize.call(this);
+  },
+
+  onAdd: function(map) {
+    this.map = map;
+    this.openPopup();
+
+    var container = L.DomUtil.create('div', 'leaflet-bar');
+    var button = this._createButton('?', 'Um síðuna', '', container, this.openPopup.bind(this));
+    return container;
+  },
+
+  openPopup: function() {
+    this.popup.setLatLng(this.map.getCenter());
+    this.popup.openOn(this.map);
+  },
+
+  _createButton: L.Control.Zoom.prototype._createButton,
+});
+
+
+function loadRoutes(map, aboutControl, busControl) {
+  aboutControl.popup.loading();
+  $.getJSON(BASEURL + 'points.php', function(routes) {
+    var layers = {};
+    for(var routeId in routes) {
+      if(routeId === '')
+        continue;
+      var layer = L.layerGroup();
+      routes[routeId].forEach(function(point) {
+        L.marker(point, {icon: getIcon(routeId), clickable: false}).addTo(layer);
+      });
+      layers[routeId] = layer;
+      busControl.addRoute(routeId, layer);
+    }
+    map.addControl(busControl);
+    aboutControl.popup.loaded();
+  }).fail(function() {
+    aboutControl.popup.loadingFailed(function() {
+      loadRoutes(map, aboutControl, busControl);
+    })
+  });
+
+}
+
 window.addEventListener('load', function() {
   // create a map in the "map" div, set the view to a given place and zoom
   var map = L.map('map').setView([64.135491, -21.896149], 12);
@@ -93,20 +197,10 @@ window.addEventListener('load', function() {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+  var aboutControl = new AboutControl();
+  map.addControl(aboutControl);
 
-  $.getJSON(BASEURL + 'points.php', function(data) {
-    var layers = {};
-    var busControl = new BusControl();
-    for(var routeId in data) {
-      if(routeId === '')
-        continue;
-      var layer = L.layerGroup();
-      data[routeId].forEach(function(point) {
-        L.marker(point, {icon: getIcon(routeId), clickable: false}).addTo(layer);
-      });
-      layers[routeId] = layer;
-      busControl.addRoute(routeId, layer);
-    }
-    map.addControl(busControl);
-  });
+  var busControl = new BusControl();
+
+  loadRoutes(map, aboutControl, busControl);
 });
